539fe93fc61e114a3ddfdb31aebc6f7d1bb89ed3
Bernardo Amorim <contato@bamorim.com>
2025-01-09 21:50:53 +0000

    Accept concurrent connections
    Probably I should think about limiting the number of goroutines, but
    I'll ignore it for now

--- app/server.go
+++ app/server.go
@@ -9,22 +9,7 @@ import (
 
 const pong = "+PONG\r\n"
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	// Create a buffer to read data to
@@ -43,5 +28,26 @@ func main() {
 		if bytes.Contains(b, []byte("\n")) {
 			conn.Write([]byte(pong))
 		}
+	}
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 }
